7c1bc3ed39f9399ab28517256bd7cf2066d5aa2e
joejjara <joejjara@gmail.com>
2026-02-11 13:01:53 -0800

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ noBtn.addEventListener("mouseover", () => {
 // YES is clicked
 
 yesBtn.addEventListener("click", () => {
-    title.textContent = "Yayyy!";
+    title.textContent = "↓ You and me! ↓";
 
     catImg.src = "cat_dance.gif";
 
